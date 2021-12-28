42da3aa8c579ad64663deb1e08d6fae94cb39984
Lisa Staehli <lisa.staehli@hotmail.com>
2021-12-28 20:59:25 +0100

    initial commit with WebMap & start page

---
+++ components/webmap.js
@@ -0,0 +1,48 @@
+import WebMap from "@arcgis/core/WebMap";
+import MapView from "@arcgis/core/views/MapView";
+import Legend from "@arcgis/core/widgets/Legend";
+
+import { useState, useEffect, useRef } from "react";
+
+export default function Map({ portalItemId }) {
+  const [viewLoaded, setViewLoaded] = useState(false);
+  const viewRef = useRef();
+
+  async function loadMap() {
+    if (viewRef.current) {
+      const webmap = new WebMap({
+        portalItem: {
+          id: portalItemId,
+        },
+      });
+
+      console.log(viewRef.current.id);
+      const view = new MapView({
+        map: webmap,
+        container: viewRef.current.id,
+      });
+
+      view.when(() => setViewLoaded(true));
+
+      const legend = new Legend({
+        view: view
+      });
+      
+      view.ui.add(legend, "bottom-right");
+    }
+  }
+
+  useEffect(async () => {
+    if (!viewLoaded) {
+      await loadMap();
+    }
+  }, []);
+
+  return (
+    <div
+      style={{ height: "100%", width: "100%" }}
+      id="viewDiv"
+      ref={viewRef}
+    ></div>
+  );
+}
---
+++ pages/maps/climate-change.js
@@ -0,0 +1,187 @@
+import Link from "next/link";
+import Head from "next/head";
+import Map from "../../components/webmap";
+
+export default function ClimateChangeMap() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Global Data Exploration - Climate Change Map</title>
+        <link
+          href="https://js.arcgis.com/4.22/@arcgis/core/assets/esri/themes/light/main.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <main>
+        <div className="card">
+          <Link href="/">
+            <a>
+              <h3 style={{ margin: 0 }}>&larr; Back</h3>
+            </a>
+          </Link>
+        </div>
+        <h1 className="title">Climate Change Map</h1>
+      </main>
+      <div id="mapContainer">
+        <Map
+          portalItemId="6e01f2273d1e48ad83d786af68330f6a"
+          containerId="viewDiv"
+        />
+      </div>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        #mapContainer {
+          height: 600px;
+          width: 100%;
+        }
+
+        button {
+          display: flex;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
